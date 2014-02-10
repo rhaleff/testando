1e324fd3dc55b741324dfe5a00d684d7e091103b
Rhaleff <rhaleff.nascimento@dce.ufpb.br>
2014-02-10 12:49:28 -0300

    Coloquei um ponto final

--- src/printar.java
+++ src/printar.java
@@ -1,6 +1,6 @@
 
 public class printar {
 	public static void main(String [] args){
-		System.out.println("Rháleff Rodrigues está testando o GIT");
+		System.out.println("Rháleff Rodrigues está testando o GIT.");
 	}
 }
